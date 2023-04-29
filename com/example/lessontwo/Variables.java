c9697a28504f23d3909195755502f18acc0cf120
nzscode <93976904+nzscode@users.noreply.github.com>
2023-04-29 12:49:49 -0400

    Create Variables.java

---
+++ com/example/lessontwo/Variables.java
@@ -0,0 +1,8 @@
+package com.example.lessontwo;
+
+public class Variables {
+    public static void main(String[] args) {
+        int busPassengers = 5;
+        System.out.println(busPassengers);
+    }
+}
