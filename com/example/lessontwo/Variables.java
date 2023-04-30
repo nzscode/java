d644f9af65895e3e51f713cf532c866a51a8a7df
nzscode <93976904+nzscode@users.noreply.github.com>
2023-04-29 22:02:47 -0400

    Delete Variables.java

--- com/example/lessontwo/Variables.java
+++
@@ -1,8 +0,0 @@
-package com.example.lessontwo;
-
-public class Variables {
-    public static void main(String[] args) {
-        int busPassengers = 5;
-        System.out.println(busPassengers);
-    }
-}
