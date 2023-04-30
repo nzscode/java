0d5d51749557710fb62de7ce92fb69f3057fe3c5
nzscode <93976904+nzscode@users.noreply.github.com>
2023-04-29 22:02:09 -0400

    Create HelloWorld.java

---
+++ Section1/HelloWorld.java
@@ -0,0 +1,10 @@
+package Section1;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+       int num1 = 5;
+       int num2 = 2;
+       num1 *= num2;
+       System.out.println(num1);
+    }
+}
