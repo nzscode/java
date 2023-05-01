847a4138a2ed1d65de62b2d1b865d5578259ef79
nzscode <93976904+nzscode@users.noreply.github.com>
2023-04-30 20:24:57 -0400

    Create Detention.java

---
+++ Section4/Detention.java
@@ -0,0 +1,34 @@
+package Section4;
+
+public class Detention {
+    public static void main(String[] args) {
+
+        //what Bart did so far
+        writingLines();
+        writingLines();
+        writingLines();
+        writingLines();
+        writingLines();
+        writingLines();
+
+        // Task 2 – call the function 6 times. 
+   }
+
+
+
+    //Task 1 - Make a function here. See the doc comment for details. 
+public static void writingLines(){
+    System.out.println("I will not copy and paste code.");
+    System.out.println("I will use Camel Case when writing class names.");
+    System.out.println("I will use lower Camel Case when writing function names.");
+    System.out.println("I will use lower Camel Case when writing variables names.\n");
+}
+    /**    
+      * Funtion name: printLines
+      *
+      * Inside the function:
+      *   1. prints the four lines       
+      */
+
+
+}
