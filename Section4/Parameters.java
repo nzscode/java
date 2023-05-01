d2594041dbe3b897c940446756e26ba7e69c2f6a
nzscode <93976904+nzscode@users.noreply.github.com>
2023-04-30 20:24:55 -0400

    Create Parameters.java

---
+++ Section4/Parameters.java
@@ -0,0 +1,18 @@
+package Section4;
+import java.util.*;
+
+public class Parameters {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        calculateArea(2.3, 3.5);
+        calculateArea(1.6, 2.4);
+        calculateArea(2.6, 4.3);
+
+
+    }
+    public static void calculateArea(double length, double width){
+        double area = length * width;
+        System.out.println("the area is " + area);
+    }
+
+}
