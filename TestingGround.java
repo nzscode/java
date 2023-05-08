03af4f0b0f8a323c8584b5d21fd459a713561bc5
nzscode <93976904+nzscode@users.noreply.github.com>
2023-05-08 09:37:20 -0400

    Update TestingGround.java

--- TestingGround.java
+++ TestingGround.java
@@ -1,16 +1,14 @@
-public class TestingGround{
-    public static void main (String[] args){
-        int num1 = 10;
-        if (num1 >= 12){
-            System.out.println("num1 is larger than or equal to 10");
-        }else{
-            System.out.println("num1 is smaller than 10");
-        }
+public class TestingGround {
+    public static void main(String[] args) {
+        int i = 0;
+//        while (i < 10){
+//            System.out.println(i);
+//            i++;
+//        }
 
-
-        int num2 = 10;
-        String result = (num1 >= 8)? "num1 is larger than or equal to 10" :
-                "num1 is smaller than 10";
-        System.out.println(result);
+        do {
+            System.out.println(i);
+            i++;
+        }while (i < 10);
     }
 }
