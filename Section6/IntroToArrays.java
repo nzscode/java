0ea027b5bac012c24cd3368204205b0a1b6cdd82
nzscode <93976904+nzscode@users.noreply.github.com>
2023-05-08 09:37:10 -0400

    Create IntroToArrays.java

---
+++ Section6/IntroToArrays.java
@@ -0,0 +1,8 @@
+package Section6;
+
+public class IntroToArrays {
+    public static void main(String[] args) {
+        String[] kingdoms = {"Mercia", "Wessex", "Northumbria", "East Anglia"};
+        System.out.println(kingdoms[0]);
+    }
+}
