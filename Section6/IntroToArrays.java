038aa2e7676ec2b033065ed4550222bbf9dbfe4c
nzscode <93976904+nzscode@users.noreply.github.com>
2023-05-09 08:09:05 -0400

    Delete IntroToArrays.java

--- Section6/IntroToArrays.java
+++
@@ -1,8 +0,0 @@
-package Section6;
-
-public class IntroToArrays {
-    public static void main(String[] args) {
-        String[] kingdoms = {"Mercia", "Wessex", "Northumbria", "East Anglia"};
-        System.out.println(kingdoms[0]);
-    }
-}
