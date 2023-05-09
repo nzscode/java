51d1626311ac0fee96a91d00517873607eeb4af2
nzscode <93976904+nzscode@users.noreply.github.com>
2023-05-09 09:32:59 -0400

    Create Receipt6_8.java

---
+++ Section6/Receipt6_8.java
@@ -0,0 +1,19 @@
+package Section6;
+
+public class Receipt6_8 {
+    public static void main(String[] args) {
+
+        String[] apples = {"Gala", "Granny Smith", "Macintosh"};
+        double[] prices = {1.99, 1.49, 1.29};
+        System.out.println("Here's your receipt:\n");
+        
+        // See instructions on Learn the Part (Workbook 6.8)
+
+        for (int i = 0; i < apples.length; i++) {
+            System.out.println("\t" + apples[i] + ": $" + prices[i] + ".");
+
+        }
+        
+         // to be used in for loop.
+    }
+}
