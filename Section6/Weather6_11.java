2df890d38f774a3a3538b481b39ee9c1793d49da
nzscode <93976904+nzscode@users.noreply.github.com>
2023-05-09 12:42:46 -0400

    Create Weather6_11.java

---
+++ Section6/Weather6_11.java
@@ -0,0 +1,18 @@
+package Section6;
+
+import java.util.Arrays;
+
+public class Weather6_11 {
+    public static void main(String[] args) {
+        
+        // The instructions for this workbook are on Learn the Part (Workbook 6.11)
+        double[] celsius = {12.5, 14.5, 17.0, 21.0, 23.0, 18.5, 20.0};
+        double[] fahrenheit;
+        
+
+    }
+    public static double[] celciusToFahrenheit(double[] celcius){
+        double[] farhrenheit = Arrays.copyOf(celcius, )
+    }
+
+}
