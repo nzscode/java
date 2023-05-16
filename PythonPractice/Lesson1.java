9e527f426d0c372601417be4bd3d5058a0c6684f
nzscode <93976904+nzscode@users.noreply.github.com>
2023-05-16 08:21:36 -0400

    Create Lesson1.java

---
+++ PythonPractice/Lesson1.java
@@ -0,0 +1,16 @@
+package PythonPractice;
+
+public class Lesson1 {
+    public static void main(String[] args) {
+        System.out.println("Printing: Lesson 1.");
+        System.out.println("Printing is fun!\nJava > Python.\nI spilled Java all over my paper.\nMy dog ate my Java.");
+
+        System.out.println("Printing: Lesson 2.");
+        System.out.println("N   N  ZZZZZ");
+        System.out.println("NN  N     Z ");
+        System.out.println("N N N    Z  ");
+        System.out.println("N   N   Z   ");
+        System.out.println("N   N  Z    ");
+        System.out.println("N   N  ZZZZZ");
+    }
+}
